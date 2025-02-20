fd41d4af371f550ff9be0788380c23ae141e0b89
xxaksed <aksed.336@mail.ru>
2025-02-20 17:41:14 +0300

    lesson4hw

--- index.js
+++ index.js
@@ -1,59 +1,49 @@
-//algo #1 
-nums = [10, 22, 28, 29, 30, 40, 10, 23, 5, 7]
-
-function findPairClosNum(array, target) {
-    array.sort((a, b) => a - b);
-    let left = 0;
-    let right = array.length - 1;
-    let closestSum = Infinity;
-    let closestPair = [];
-    while (left < right) {
-        if (Math.abs(array[left] + array[right] - target) < Math.abs(closestSum - target)) {
-            closestSum = array[left] + array[right];
-            closestPair = [array[left], array[right]];
+var array = [
+      [1,   2,  3, 4],
+      [12, 13, 14, 5],
+      [11, 16, 15, 6],
+      [10,  9,  8, 7]
+    ];
+    
+    function spiralMatrex(array) {
+        var widthPoint = 0;
+        var hightPoin = 0;
+        var w = array[0].length - 1;
+        var h = array.length - 1;
+        var direction = 1;
+        var result = [];
+        var mapPoint = new Map();
+        var quantity = 0;
+    
+        while (quantity <= (w + 1) * (h + 1)) { 
+            mapPoint.set(`${widthPoint},${hightPoin}`); 
+            result.push(array[hightPoin][widthPoint]);
+    
+            if (direction === 1 && widthPoint < w && !mapPoint.has(`${widthPoint + 1},${hightPoin}`)) {
+                widthPoint++;
+            } else if (direction === 1) {
+                direction = 2;
+            }
+            if (direction === 2 && hightPoin < h && !mapPoint.has(`${widthPoint},${hightPoin + 1}`)) {
+                hightPoin++;
+            } else if (direction === 2) {
+                direction = 3;
+            }
+            if (direction === 3 && widthPoint > 0 && !mapPoint.has(`${widthPoint - 1},${hightPoin}`)) {
+                widthPoint--;
+            } else if (direction === 3) {
+                direction = 4;
+            }
+            if (direction === 4 && hightPoin > 0 && !mapPoint.has(`${widthPoint},${hightPoin - 1}`)) {
+                hightPoin--;
+            } else if (direction === 4) {
+                direction = 1;
+            }
+    
+            quantity++;
         }
-        if (array[left] + array[right] < target) {
-            left++;
-        } else {
-            right--;
-        }
+    
+        console.log(result);
     }
-    console.log(closestPair)
-    console.log(closestSum)
-}
-
-findPairClosNum(nums, 54)
-
-//algo #2 
-s = "swiss"
-function findPairClosNum(array) {
-    let left = 0;
-    let right = array.length - 1;
-    while (left < right) {
-        if (array[left] !== array[right]) {
-            right--
-        }
-        else {
-            left++
-            right = array.length - 1;
-        }
-    }
-    console.log(array[left])
-}
-
-findPairClosNum(s)
-
-//algo #3 
-s1 = "bottlewater"
-s2 = "erbottlewat"
-
-function isRotation(str1, str2) {
-    const concatenated = str1 + str1;
-    if (concatenated.includes(str2)) {
-        console.log(true)
-    }
-    else {
-        console.log(false)
-    }
-}
-isRotation(s1, s2)
+    
+    spiralMatrex(array);
